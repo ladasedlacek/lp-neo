e8e559b0711898ee292401c8bdb024897d8ff215
ladasedlacek <sedlacek.lada@gmail.com>
2022-12-21 21:01:43 +0100

    Validation not finished

--- js/f__form-validation.js
+++ js/f__form-validation.js
@@ -6,10 +6,43 @@ const form_phone = document.getElementById('lpForm_telephone')
 const form_message = document.getElementById('lpForm_message')
 const form_gdpr = document.getElementById('lpForm_gdpr')
 const form_submit = document.getElementById('lpForm_submit')
+const verify_class = 'lpInputVerrify'
+const error_class = 'lpForm__errorMessage--active'
+const input_stack = [form_company_name, form_name, form_company_number]
 
-// company name validation
-form_company_name.addEventListener('keyup', function() {
-    let input_length = this.value
-    input_length.length >= 3 ? console.log(true) : console.log(false)
+// validation - company name / name / company number
+input_stack.forEach(element => {
+    element.addEventListener('keyup', function() {
+        let input_length = this.value
+        let label = this.previousElementSibling
+        let error_message = this.nextElementSibling
+
+        input_length.length >= 3 ? (label.classList.add(verify_class), error_message.classList.remove(error_class)) : 0
+        label.classList.contains(verify_class) === true && input_length.length < 3 ? (label.classList.remove(verify_class), error_message.classList.add(error_class)) : 0
+    })
 })
 
+// validation - phone
+form_phone.addEventListener('keyup', function() {
+    let input_length = this.value
+    let label = this.previousElementSibling
+    let error_message = this.nextElementSibling
+
+    input_length.length >= 8 ? (label.classList.add(verify_class), error_message.classList.remove(error_class)) : 0
+    label.classList.contains(verify_class) === true && input_length.length < 8 ? (label.classList.remove(verify_class), error_message.classList.add(error_class)) : 0
+})
+
+// validation - email
+form_email.addEventListener('keyup', function() {
+    let input_length = this.value
+    let label = this.previousElementSibling
+    let error_message = this.nextElementSibling
+    input_length = input_length.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+
+    input_length === null ? 0 : (label.classList.add(verify_class), error_message.classList.remove(error_class))
+    input_length === null && label.classList.contains(verify_class) === true ? (label.classList.remove(verify_class), error_message.classList.add(error_class)) : 0
+})
+
+/* form_submit.addEventListener('click', function() {
+
+}) */
--- js/f__menu.js
+++ js/f__menu.js
@@ -1,18 +1,26 @@
+const navigation = document.querySelector('.lpNav')
+const navigation_items = document.querySelector('.lpNav__right .lpNav__items')
 const menu_button = document.querySelector('.lpNav__menuIcon')
-const menu_target = document.querySelector('.lpNav__menu')
+const menu_wrapper = document.querySelector('.lpNav__wrapper')
+const menu_target = document.querySelector('.lpNav__mobileMenu')
 const menu_svg = document.querySelector('.lpMenuSvg')
+const right_side = document.querySelector('.lpNav__right')
+const mobile_button = `<a href="#" class="lpNav__menuIcon lpNav__menuIcon--open">
+<svg><use class="lpMenuSvg" xlink:href="#icon-menu"></use></svg>
+</a>`
 
+// open / close
 menu_button.addEventListener('click', function(event) {
     event.preventDefault()
     const open_menu = () => {
-        menu_target.classList.add('lpNav__menu--open')
+        menu_target.classList.add('lpNav__mobileMenu--open')
         menu_svg.setAttribute('xlink:href', '#icon-close')
         menu_button.classList.remove('lpNav__menuIcon--open')
         menu_button.classList.add('lpNav__menuIcon--close')
     }
 
     const close_menu = () => {
-        menu_target.classList.remove('lpNav__menu--open')
+        menu_target.classList.remove('lpNav__mobileMenu--open')
         menu_svg.setAttribute('xlink:href', '#icon-menu')
         menu_button.classList.add('lpNav__menuIcon--open')
         menu_button.classList.remove('lpNav__menuIcon--close')
