62eae3bdb952fa58ecdefc2723f048ad62cb1525
ladasedlacek <sedlacek.lada@gmail.com>
2022-12-31 23:56:22 +0100

    Added blurs, validation of the form is not finished

--- js/f__form-validation.js
+++ js/f__form-validation.js
@@ -1,21 +1,20 @@
+const form = document.getElementById('lpForm')
 const form_company_name = document.getElementById('lpForm_company_name')
 const form_name = document.getElementById('lpForm_name')
 const form_company_number = document.getElementById('lpForm_company_number')
 const form_email = document.getElementById('lpForm_email')
 const form_phone = document.getElementById('lpForm_telephone')
-const form_message = document.getElementById('lpForm_message')
-const form_gdpr = document.getElementById('lpForm_gdpr')
-const form_submit = document.getElementById('lpForm_submit')
+const form_button = document.getElementById('lpForm_button')
+const identity = [form_company_name, form_name, form_company_number]
 const verify_class = 'lpInputVerrify'
 const error_class = 'lpForm__errorMessage--active'
-const input_stack = [form_company_name, form_name, form_company_number]
 
 // validation - company name / name / company number
-input_stack.forEach(element => {
+identity.forEach(element => {
     element.addEventListener('keyup', function() {
         let input_length = this.value
         let label = this.previousElementSibling
-        let error_message = this.nextElementSibling
+        let error_message = this.nextElementSibling 
 
         input_length.length >= 3 ? (label.classList.add(verify_class), error_message.classList.remove(error_class)) : 0
         label.classList.contains(verify_class) === true && input_length.length < 3 ? (label.classList.remove(verify_class), error_message.classList.add(error_class)) : 0
@@ -43,6 +42,34 @@ form_email.addEventListener('keyup', function() {
     input_length === null && label.classList.contains(verify_class) === true ? (label.classList.remove(verify_class), error_message.classList.add(error_class)) : 0
 })
 
-/* form_submit.addEventListener('click', function() {
+// check validation every 100 ms
+function check_validation() {
+    const button_allowed = () => {
+        form_button.classList.remove('btn--dis')
+        form_button.removeAttribute('disabled')
+    }
 
-}) */
+    const button_disabled = () => {
+        form_button.classList.contains('btn--dis') ? 0 : (form_button.classList.add('btn--dis'), form_button.disabled = true)
+    }
+
+    form.checkValidity() === true ? button_allowed() : button_disabled()
+}
+setInterval(check_validation, 100)
+
+// submit form
+function form_event(event) { 
+    event.preventDefault() 
+    clearInterval(check_validation)
+    console.log('Formulář odeslán')
+} 
+form.addEventListener('submit', form_event)
+
+// form reset
+document.querySelector('.lpForm__statusMessage .body-2--link').addEventListener('click', function(event) {
+    event.preventDefault() 
+    form.reset()
+    setInterval(check_validation, 100)
+    document.querySelector('.lpForm__statusMessage').classList.remove('lpForm__statusMessage--active')
+    form.classList.add('lpForm__wrapper--active')
+})
