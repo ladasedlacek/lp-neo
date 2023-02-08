743b3db5b9d338bba155f6b884aeff0e96cd905a
ladasedlacek <sedlacek.lada@gmail.com>
2023-02-08 18:56:36 +0100

    Helpdesk - fixed (removed window on load)

--- js/f__helpdesk.js
+++ js/f__helpdesk.js
@@ -1,52 +1,35 @@
-window.onload = () => {
-    const run_helpdesk = () => {
-        const helpdesk_stack = document.querySelectorAll('#landingpage .js-lp-showSendHelpdeskQuestionDialog')
-        helpdesk_stack.forEach(element => {
-            element.onclick = event => {
-                event.preventDefault()
-                console.log('Clicked - ' + element)
-                const question_id = element.getAttribute('data-question-id')
-                new Promise((resolve) => {
-                    HelpDeskQuestion.showSendHelpdeskQuestionDialog({
-                        questionGroupId: 34,
-                        questionId: question_id,
-                        showGroupQuestions: true,
-                        forceQuestionNameAsTitle: false
-                    })
-                    resolve()
-                }).then(() => setTimeout(() => {
-                    // remove items from the form
-                    const items_stack = ['heldeskQuestion-399', 'heldeskQuestion-192', 'heldeskQuestion-190', 'heldeskQuestion-191', 'heldeskQuestion-193', 'heldeskQuestion-194']
-                    
-                    items_stack.forEach((item) => {
-                        if (question_id == 192 && item == 'heldeskQuestion-192') {
-                            return
-                        } else if (question_id == 191 && item == 'heldeskQuestion-190' || question_id == 191 && item == 'heldeskQuestion-191') {
-                            return
-                        } else {
-                            document.getElementById(item).remove()
-                        }
-                    })
-                }, 100))
-            }
-        })
-    }
-    
-    window.location.href.indexOf('mbeta.alza.cz') != -1 || window.location.href.indexOf('m.alza.cz') != -1 ? 0 : run_helpdesk()
+const run_helpdesk = () => {
+    const helpdesk_stack = document.querySelectorAll('#landingpage .js-lp-showSendHelpdeskQuestionDialog')
+    helpdesk_stack.forEach(element => {
+        element.onclick = event => {
+            event.preventDefault()
+            console.log('Clicked - ' + element)
+            const question_id = element.getAttribute('data-question-id')
+            new Promise((resolve) => {
+                HelpDeskQuestion.showSendHelpdeskQuestionDialog({
+                    questionGroupId: 34,
+                    questionId: question_id,
+                    showGroupQuestions: true,
+                    forceQuestionNameAsTitle: false
+                })
+                resolve()
+            }).then(() => setTimeout(() => {
+                // remove items from the form
+                const items_stack = ['heldeskQuestion-399', 'heldeskQuestion-192', 'heldeskQuestion-190', 'heldeskQuestion-191', 'heldeskQuestion-193', 'heldeskQuestion-194']
+                
+                items_stack.forEach((item) => {
+                    if (question_id == 192 && item == 'heldeskQuestion-192') {
+                        return
+                    } else if (question_id == 191 && item == 'heldeskQuestion-190' || question_id == 191 && item == 'heldeskQuestion-191') {
+                        return
+                    } else {
+                        document.getElementById(item).remove()
+                    }
+                })
+            }, 100))
+        }
+    })
 }
 
-
-
-document.addEventListener('click', function(event) {
-    if (!event.target.matches('.js-lp-showSendHelpdeskQuestionDialog')) return;
-    event.preventDefault();
-    let button = event.target;
-    let question_id = button.getAttribute('data-question-id');
-    HelpDeskQuestion.showSendHelpdeskQuestionDialog({
-      questionGroupId: 34,
-      questionId: question_id,
-      showGroupQuestions: true,
-      forceQuestionNameAsTitle: false
-    });
-  }, false);
+window.location.href.indexOf('mbeta.alza.cz') != -1 || window.location.href.indexOf('m.alza.cz') != -1 ? 0 : run_helpdesk()
   
