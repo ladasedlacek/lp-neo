cb13f31bdb05c047b893e6dc11a20a8fc9d3e3b9
Ladislav Sedláček <sedlacek.lada@gmail.com>
2023-03-22 15:47:05 +0100

    Fixed toggle switcher in the usp

--- js/f__toggle.js
+++ js/f__toggle.js
@@ -3,9 +3,8 @@ const content_toggle = () => {
     const content_button = document.querySelector('.body-1--button')
     
     content_button.addEventListener('click', function(event) {
-        console.log('clicked')
         event.preventDefault()
-        console.log('clicked')
+        const content_element = document.querySelector('.lpUsps__box .perex span')
         if (content_element.classList.contains('lpUsps__box--more')) {
             content_element.classList.remove('lpUsps__box--more')
             content_element.classList.add('lpUsps__box--less')
