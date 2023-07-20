0516c75211fdc3f9d256a397350a880c53ba0677
Ladislav Sedláček <sedlacek.lada@gmail.com>
2023-07-20 16:39:52 +0200

    Helpdesk js fixed

--- js/f__helpdesk.js
+++ js/f__helpdesk.js
@@ -3,95 +3,45 @@ const run_helpdesk = () => {
     helpdesk_stack.forEach(element => {
         element.onclick = event => {
             event.preventDefault()
-            console.log('Clicked - ' + element)
             const question_id = element.getAttribute('data-question-id')
             new Promise((resolve) => {
-                HelpDeskQuestion.showSendHelpdeskQuestionDialog({
-                    questionGroupId: 34,
-                    questionId: question_id,
-                    showGroupQuestions: true,
-                    forceQuestionNameAsTitle: false
-                })
+                const question_id = element.getAttribute('data-question-id')
+                if (window.location.href.includes("m.alza") || window.location.href.includes("mbeta.alza")) {
+                    Alza.Mobile.Components.Partial.Dialogs.Contact.HelpdeskQuestionDetailDialog.show(34, question_id, false)
+                } else {
+                    HelpDeskQuestion.showSendHelpdeskQuestionDialog({
+                        questionGroupId: 34,
+                        questionId: question_id,
+                        showGroupQuestions: true,
+                        forceQuestionNameAsTitle: false
+                    })
+                }
                 resolve()
             }).then(() => setTimeout(() => {
-                const buttons = document.querySelectorAll('.modalSwitcher__button')
-                buttons.forEach(button => {
-                    button.addEventListener('click', () => {
-                        const event = button.getAttribute('data-event')
-                        const classButton = "modalSwitcher__button--active"
-                        const classContent = "modalNewContent--show"
-
-                        if (!button.classList.contains(classButton)) {
-                            // Button switcher
-                            const activeButton = document.querySelector('.' + classButton)
-                            activeButton.classList.remove(classButton)
-                            button.classList.add(classButton)
-
-                            // Content switcher
-                            const contentActive = document.querySelector('.' + classContent)
-                            contentActive.classList.remove(classContent)
-                            const findContent = document.querySelector('.' + event)
-                            findContent.classList.add(classButton)
-                        }
+                if (question_id == '191') {
+                    const buttons = document.querySelectorAll('.modalSwitcher__button')
+                    buttons.forEach(button => {
+                        button.addEventListener('click', () => {
+                            const buttonClasses = button.getAttribute('class')
+                            const event = button.getAttribute('data-event')
+                            const classButton = "modalSwitcher__button--active"
+                            const classContent = "modalNewContent--show"
+    
+                            if (!buttonClasses.includes(classButton)) {
+                                const activeButton = document.querySelector('.' + classButton)
+                                activeButton.classList.remove(classButton)
+                                button.classList.add(classButton)
+    
+                                const contentActive = document.querySelector('.' + classContent)
+                                contentActive.classList.remove(classContent)
+                                const findContent = document.querySelector('.' + event)
+                                findContent.classList.add(classContent)
+                            }
+                        })
                     })
-                })
+                }
             }, 100))
         }
     })
 }
 run_helpdesk()
-  
-
-
-const kokotarium = () => {
-    const buttons = document.querySelectorAll('.modalSwitcher__button')
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const event = button.getAttribute('data-event')
-            const classButton = "modalSwitcher__button--active"
-            const classContent = "modalNewContent--show"
-            console.log(button)
-
-            console.log(!button.classList.contains(classButton))
-
-            if (!button.classList.contains(classButton)) {
-                console.log("ahoj")
-                // Button switcher
-                const activeButton = document.querySelector('.' + classButton)
-                console.log(activeButton)
-                activeButton.classList.remove(classButton)
-                button.classList.add(classButton)
-                console.log("click 2")
-
-                // Content switcher
-                const contentActive = document.querySelector('.' + classContent)
-                contentActive.classList.remove(classContent)
-                const findContent = document.querySelector('.' + event)
-                findContent.classList.add(classButton)
-                console.log("click 3")
-            }
-        })
-    })
-}
-kokotarium()
-
-const ahoj = () => {
-    const buttons = document.querySelectorAll('.modalSwitcher__button')
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const buttonClasses = button.getAttribute('class')
-            console.log(buttonClasses)
-            const event = button.getAttribute('data-event')
-            const classButton = "modalSwitcher__button--active"
-            const classContent = "modalNewContent--show"
-
-            if (!buttonClasses.includes(classButton)) {
-                console.log("Neobsahuje")
-            } else {
-                console.log("Obsahuje")
-            }
-        })
-    })
-}
-ahoj()
-    
